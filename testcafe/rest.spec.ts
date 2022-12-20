aa488122c852aead3aa1bcccd0b7763028f4e90e
Roy Wang <roywang4212@gmail.com>
2022-12-20 09:00:49 -0600

    remove debug

--- testcafe/rest.spec.ts
+++ testcafe/rest.spec.ts
@@ -5,7 +5,6 @@ fixture("Rest").page`http://localhost:3000/account`;
 test("should navigate to /account2 page", async (t) => {
   await t
     .expect(screen.findByRole('heading',{ name: 'Screen1' }).exists).ok('Page1 loaded')
-    .debug()
     .click(screen.getByRole("button", { name: /navigate/i }))
 
   // Navigate to the `/account2` screen
