2c4f719d84b57965e16bce181909a08ffe89e3cb
Roy Wang <roywang4212@gmail.com>
2022-12-23 06:11:03 -0600

    update the test title

--- testcafe/rest.spec.ts
+++ testcafe/rest.spec.ts
@@ -2,10 +2,7 @@ import { screen } from "@testing-library/testcafe";
 
 fixture("Rest").page`http://localhost:3000/`;
 
-test("should do a GET", async (t) => {
+test("should render the page correctly", async (t) => {
   await t
     .expect(screen.findByRole('heading',{ name: 'Loading...' }).exists).ok('Page loaded')
-    // .click(screen.getByRole("button", { name: /fetch/i }))
-    // .wait(500)
-    // .expect(screen.getByRole("heading", { name: 'Success' }).exists).ok('Data Fetch Success')
 });
