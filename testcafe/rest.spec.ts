4166139d610f8f70db85cb179c4e23e152bf6cf2
Roy Wang <roywang4212@gmail.com>
2022-12-27 10:14:27 -0600

    clean up the codes

--- testcafe/rest.spec.ts
+++ testcafe/rest.spec.ts
@@ -1,10 +1,6 @@
 import { screen } from "@testing-library/testcafe";
 
-fixture("Rest").page`http://localhost:3000/account/login/test`;
-
-// test("heading", async t => {
-//   await t.expect(screen.findByText(/example app|loading/i).exists).ok();
-// });
+fixture("Rest").page`http://localhost:3000/account`;
 
 test("should do a GET", async (t) => {
   await t
@@ -13,29 +9,11 @@ test("should do a GET", async (t) => {
     .wait(500)
     .expect(screen.getByRole("heading", { name: 'Success' }).exists).ok('Data Fetch Success')
 });
-// test("should do a DELETE", async (t) => {
-//   await t
-//     .expect(screen.findAllByRole("heading", { name: /foo|bar/ }).count)
-//     .eql(2)
-//     .hover(screen.findByRole("heading", { name: "foo" }))
-//     .click(screen.findByRole("button", { name: /done/i }))
-//     .expect(screen.findAllByRole("heading", { name: /foo|bar/ }).count)
-//     .eql(1);
-// });
-// test("should do a POST", async (t) => {
-//   await t
-//     .typeText(screen.findByRole("textbox"), "baz")
-//     .pressKey("enter")
-//     .expect(screen.findByRole("heading", { name: "baz" }).exists)
-//     .ok();
-// });
-//
-// test("should do a PUT", async (t) => {
-//   await t
-//     .hover(screen.findByRole("heading", { name: "bar" }))
-//     .click(screen.findByRole("button", { name: /edit/i }))
-//     .typeText(screen.findByDisplayValue("bar"), "qux")
-//     .pressKey("enter")
-//     .expect(screen.findByRole("heading", { name: "barqux" }).exists)
-//     .ok();
-// });
+
+test("should do a GET2", async (t) => {
+  await t
+    .expect(screen.findByRole('heading',{ name: 'Loading...' }).exists).ok('Page loaded')
+    .click(screen.getByRole("button", { name: /fetch/i }))
+    .wait(500)
+    .expect(screen.getByRole("heading", { name: 'Success' }).exists).ok('Data Fetch Success')
+});
